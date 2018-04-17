95fba607d3e0610aaa62e60cf22d3fed678bcef9
Louis Crawley <louis_crawley@hotmail.co.uk>
2018-04-17 15:33:07 +0100

    Initial Commit

---
+++ CheckForInactiveUsers.ps1
@@ -0,0 +1,203 @@
+﻿<#
+#####################################
+Script: CheckForInactiveUsers.ps1
+Author: Louis Crawley
+Company: Trace Solutions
+Date: 23/08/2017
+Purpose: This script is to check for inactive users in the AD, list their name, last active date, O365 Licence type
+         in a csv file and send it off via email.
+Notes:
+#####################################
+#>
+
+#If filepath to store file doesn't exist, make it.
+$strFilePath = "C:\InactiveUsers"
+if(test-path $strFilePath)
+    {}
+else
+    {
+        $wshell = new-object -comobject wscript.shell
+        $wshell.popup("You need to run the file: InactiveUsersInitialRun.ps1 before this can be used",0,"Requires References",0x1)
+        exit
+    }
+
+import-module activedirectory
+
+#Get date for the file naming
+$dt = get-date -format ddMMyyyy
+
+#Get Domain from Refs
+$DOMAIN = get-content C:\InactiveUsers\References\1\DOMAIN.txt
+
+#Office 365 Connectivity
+#Create O365Credentials From Refs and store in $365Credentials variable as PSCredential
+$365User = get-content C:\InactiveUsers\References\3\1\Cred1.txt
+$365Pass = get-content C:\InactiveUsers\References\3\2\Cred2.txt
+#$365Pass = "Puga9131"
+$365Pass = ConvertTo-SecureString "$365Pass" -AsPlainText -force
+$365Credentials = New-Object system.management.automation.pscredential($365User,$365Pass)
+
+write-host $365pass -ForegroundColor Yellow
+###
+#Use O365 Credentials to connect
+import-module msonline
+$365PSSession = New-PSSession -ConfigurationName microsoft.exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $365Credentials -Authentication basic -AllowRedirection
+Import-PSSession $365PSSession -AllowClobber
+Connect-MsolService -Credential $365Credentials
+
+#Set how long to check for users being inactive
+$InactiveDays = new-timespan -days 30
+#Setup Exempt Group from AD Search
+#$ExemptionGroup = get-adgroup ".TPS Blue Team" | select CN | out-string
+$SEARCHAD = search-adaccount -usersonly -accountinactive -timespan $InactiveDays | get-aduser | Where-Object {$_.Enabled -eq $true} | select-object name
+$InactiveUsers = $SEARCHAD | Out-String
+$Inactiveusers = $InactiveUsers.Split("`n",[system.stringsplitoptions]::RemoveEmptyEntries)
+
+#Couple of static variables
+$TS = "Solutions"
+$SMTPServ = get-content C:\InactiveUsers\References\4\SMTP.txt
+#$TPS = 'Payroll'
+#$SolutionsMember = get-adgroupmember -Identity $TS -Recursive | select -ExpandProperty name
+#$PayrollMember = get-adgroupmember -Identity "*$TPS*" -Recursive | select -ExpandProperty name
+
+#Loop through the Inactive AD users, and query the O365 account for the licence type associated with the user then assign to $ExportingItems variable
+#write-host $InactiveUsers -ForegroundColor Yellow
+
+$ExportingItems = ''
+$ExportingItems = @()
+
+ForEach($line in $InactiveUsers)
+{
+    $Licence = ""
+    $LicenceType = ""
+    $InactiveTime = ""
+
+    if ($line -ne $null)
+    {
+        $Licence = (Get-MsolUser -SearchString $line).licenses.servicestatus | Out-String -ErrorAction SilentlyContinue
+            if($Licence.Contains("EXCHANGE_S_ENTERPRISE"))
+            {
+                $LicenceType = "E3 O365 Licence"
+            }
+            elseif($Licence.Contains("EXCHANGE_S_STANDARD"))
+            {
+                $LicenceType = "E1 O365 Licence"
+            }
+            else
+            {
+                $LicenceType = "Unlicenced"
+            }
+    }
+    else
+    {
+        $LicenceType = "Unlicenced"
+    }
+
+    
+<#   if ($SolutionsMember -contains $line)
+        {
+        $GroupName = 'Solutions'
+        }
+    elseif ($PayrollMember -contains $line)
+        {
+        $GroupName = 'Payroll Services'
+        }
+    else
+        {
+        $GroupName = 'Other'
+        }
+#>
+
+
+    $NameForExport = $line
+    $LicenceForExport = $LicenceType
+
+    $ExportingItems += new-object psobject -Property @{
+                                    Name = $NameForExport
+                                    LicenceType = $LicenceForExport
+                                    #Company = $GroupName
+                                                      }
+
+
+}
+
+
+#write-host 'Please let this work ლ(ಥ Д ಥ )ლ otherwise I will kill myself (つ◉益◉)つ ' -ForegroundColor Yellow
+
+#Change the $ExportingItems variable to an object, and export to csv file
+$ExportingItems = $ExportingItems | Select-Object Name, LicenceType | Sort-Object -property licencetype, name #, GroupName
+$ExportingItems | export-csv "C:\InactiveUsers\Exports\Inactive_Users_$dt.csv" -notypeinformation
+
+
+#Write-Host 'Why you no work script!?!? └༼ ಥ ᗜ ಥ ༽┘ └༼ ಥ ᗜ ಥ ༽┘ └༼ ಥ ᗜ ಥ ༽┘' -ForegroundColor Red
+
+#Email CSV file to relevent people.
+
+
+################ TEST #####################
+
+#$Mailbox = get-mailbox -Identity 'Louis Crawley' | out-string
+#write-host $Mailbox -ForegroundColor Cyan
+
+#$Test = $Mailbox.name
+#write-host $Test -ForegroundColor Yellow
+
+$MailingList = get-content C:\InactiveUsers\References\4\MailingList.txt
+
+
+#Set a variable with the time of day for email body text
+if( (get-date -uformat %p) -eq "AM") {
+    $Time = "Morning"
+    }
+else {
+    $Time = "Afternoon"
+    }
+
+$Attachment = "C:\InactiveUsers\Exports\Inactive_Users_$dt.csv"
+
+
+#Does not work when running from Powershell command line, only in ISE
+
+#Create an outlook session for emailing
+add-type -assembly "Microsoft.Office.Interop.Outlook"
+$ol = new-object -ComObject outlook.application
+$ns = $ol.getnamespace("MAPI")
+
+$BodyText = "Good $time," + "`r`n" + "`r`n" + "Please find attached a current list of Inactive AD users on the " + "'" + "$DOMAIN" + "'" + " domain, and their respective Office365 licences." + "`r`n" + "`r`n" + "Regards," + "`r`n" + "`r`n" + "TST"
+
+
+$mail = $ol.createitem(0)
+$mail.to = $mail.recipients.add($MailingList)
+$mail.subject = "Current Inactive Users - $DOMAIN"
+$mail.body = $BodyText
+$mail.Attachments.add($Attachment)
+$mail.Send()
+
+#$inspector = $mail.getinspector
+#$inspector.activate()
+
+
+
+<# TEST
+$SendTo = "louis.crawley@tracesolutions.co.uk"
+
+$Msg = "Good $time," + "`r`n" + "`r`n" + "Please find attached a current list of Inactive AD users on the " + "'" + "$DOMAIN" + "'" + " domain, and their respective Office365 licences." + "`r`n" + "`r`n" + "Kind Regards," + "`r`n" + "`r`n" + "Technical Services Team" + "`r`n" + "Trace Solutions - Part of Trace Group"
+
+$EmailFrom = "$365user"
+$Subject = "Current Inactive Users - $domain"
+$smtpserver = "$365User"
+$smtp = new-object net.mail.smtpclient($smtpserver)
+$smtp.send($emailfrom, $sendto, $Subject, $Msg)
+#>
+
+<#
+write-host $365user
+
+
+$Msg = "Good $time," + "`r`n" + "`r`n" + "Please find attached a current list of Inactive AD users on the " + "'" + "$DOMAIN" + "'" + " domain, and their respective Office365 licences." + "`r`n" + "`r`n" + "Kind Regards," + "`r`n" + "`r`n" + "Technical Services Team" + "`r`n" + "Trace Solutions - Part of Trace Group"
+
+Send-MailMessage -To $MailingList -from $365User -SmtpServer "$SMTPServ" -Subject "Current Inactive Users - $domain" -Attachments $attachment -body $msg -Credential $365Credentials
+#>
+
+
+#Eliminate unlicenced users, and blank user names from the end result.
---
+++ InactiveUsersInitialRun.ps1
@@ -0,0 +1,114 @@
+﻿<#
+########################################
+Script: InactiveUsersInitialRun.ps1
+Author: Louis Crawley
+Company: Trace Solutions
+Date: 23/08/2017
+Purpose: This script is to setup any references for the Inactive users script, and to make the inactive users script
+         into a scheduled task
+Notes: Please run as administrator
+       This script can be run as many times as you want, it will replace any references with new information when run. 
+########################################
+#>
+
+#Set execution policy to remotesigned
+if((Get-ExecutionPolicy) -ne "RemoteSigned")
+    {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force}
+     
+
+#If filepath to store txt file doesn't exist, make it.
+$strFilePath = "C:\InactiveUsers\Exports\"
+if(test-path $strFilePath)
+    {}
+else
+    {New-Item -itemtype directory -force -path C:\InactiveUsers\Exports}
+
+
+
+$DomainName = (Get-WmiObject win32_computersystem).domain
+
+#Loads a vbnet inputbox for Domain Name
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$DomName = [Microsoft.VisualBasic.Interaction]::InputBox("Please type in your domain name (case-sensetive) - Suggested:" + "`r`n" + "       " + $domainname, "DOMAIN", "$DomName")
+
+#Loads a vbnet inputbox for Admin Name
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$AdminName = [Microsoft.VisualBasic.Interaction]::InputBox("Please type in a user who's password won't change (Local System/ Local Service/ Network Service etc)", "Admin Username", "$AdminName")
+
+#Loads a vbnet inputbox for Domain Name
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$AdminPassword = [Microsoft.VisualBasic.Interaction]::InputBox("Please type in the password for user:" + "`r`n" + "       " + $AdminName, "Admin Password", "$AdminPassword")
+
+#Office365 Logon Credentials
+$wshell = new-object -comobject wscript.shell
+$wshell.popup("Please Enter Email Address/Pass for Account to Email From (Must have sufficient permissions to check licences",0,"Office365 Credentials",0x1)
+$365Login = get-credential
+$365LoginUser = $365Login.GetNetworkCredential().UserName
+$365LoginPass = $365Login.GetNetworkCredential().Password
+
+$FullUser = "$AdminName@$domainname"
+
+#Input box for Email Addresses to fire the information to
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$EmailList1 = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the email address for the recipient of the script results (1 of 2):" + "`r`n" + "       " + $EmailList11, "Email Address 1", "$EmailList1")
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$EmailList2 = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the email address for the recipient of the script results (2 of 2):" + "`r`n" + "       " + $EmailList22, "Email Address 2", "$EmailList2")
+
+$EmailList = "$EmailList1 , $emaillist2"
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$SMTPServ = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter your SMTP Server Address. If you are having trouble, please go to mxtoolbox.com and enter in your domain to find." + "`r`n" + "       " + $SMTPServ1, "SMTP Address", "$SMTPServ")
+
+#Create new text files referencing the Domain Name
+New-Item C:\InactiveUsers\References\1\DOMAIN.txt -type file -force -value "$domname"
+New-Item C:\InactiveUsers\References\2\1\Cred1.txt -type file -force -value "$FullUser"
+New-Item C:\InactiveUsers\References\2\2\Cred2.txt -type file -force -value "$AdminPassword"
+New-Item C:\InactiveUsers\References\3\1\Cred1.txt -type file -force -value "$365LoginUser"
+New-Item C:\InactiveUsers\References\3\2\Cred2.txt -type file -force -value "$365LoginPass"
+New-Item C:\InactiveUsers\References\4\MailingList.txt -type file -force -value "$EmailList"
+New-Item C:\InactiveUsers\References\4\SMTP.txt -type file -force -value "$SMTPServ"
+
+###Check if Scheduled Task Exists, if not continue
+
+import-module scheduledtasks
+
+$TaskName = 'Check_For_Inactive'
+
+if( Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue)
+    {Unregister-ScheduledTask -TaskName $TaskName -confirm:$true}
+
+#Create Scheduled Task#
+
+#Get Script Location
+
+$wshell.popup("Please select location of script for task",0,"Select Script Location",0x1)
+
+$initialDirectory = "C:\"
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "PSScript (*.ps1,*.psm1,*.psd1,*.ps1xml,*.pssc,*.psrc)| *.ps1;*.psm1;*.psd1;*.ps1xml;*.pssc;*.psrc"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+#    write-host $openfiledialog -ForegroundColor Green
+#    write-host $OpenFileDialog.SafeFileName -ForegroundColor Cyan
+$FileDirec = $OpenFileDialog.FileName
+$InactiveUsersScript = $OpenFileDialog.FileName
+
+#$BatchContents = "powershell.exe -file " + '"' + "$inactiveusersscript" + '"'
+#$BatchLocation = '"' + "C:\InactiveUsers\References\4\RunPS.bat"+ '"'
+#New-Item C:\InactiveUsers\References\4\RunPS.bat -type file -force -value ("$batchcontents")
+
+$ActionText = "powershell.exe"
+$ArgumentText = "-executionpolicy bypass -file " + '"' + "$inactiveusersscript" + '"'
+
+write-host $ActionText -ForegroundColor Yellow
+$Action = New-ScheduledTaskAction "$actiontext" -Argument "$argumenttext"
+$Trigger = New-ScheduledTaskTrigger -Weekly -At '1PM' -DaysOfWeek Monday
+$Principal = new-scheduledtaskprincipal -UserId $365LoginUser -RunLevel Highest
+
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings (New-ScheduledTaskSettingsSet) -Principal $principal
+
+
+$Task | Register-ScheduledTask -TaskName $TaskName -User $fulluser -Password $AdminPassword
+
+$wshell.popup("Scheduled Task setup. Please check 'Check_For_Inactive' in your schedular" + " `r`n" + "Script is finished, please close.")
